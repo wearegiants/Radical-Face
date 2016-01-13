de89859f9f14e0eff31837fc7c5e691f7e88a688
Craig Fowler <craig@wearegiants.com>
2016-01-13 13:12:41 -0800

    stuff

--- page-splash.php
+++ page-splash.php
@@ -15,9 +15,9 @@
   $wp_query->query($args); 
 ?>
 
-<div class='fs-row'>
+<div class='fs-row map-list'>
   <?php while ($wp_query->have_posts()) : $wp_query->the_post();  ?>
-  <div class="fs-cell fs-all-full" style="position:relative">
+  <div class="map-list_item fs-cell fs-all-full" style="position:relative">
     <a href="<?php the_permalink(); ?>" class="ajax-point">
       <?php the_title(); ?>
     </a>
@@ -29,5 +29,8 @@
     $wp_query = $temp;
   ?>
 </div>
+
+<div class="map-banner"></div>
+<div class="map-bg"></div>
 
 <?php get_footer(); ?>
