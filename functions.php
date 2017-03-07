88d583b0383eea7e291beb4f13edea4c44641381
Craig Fowler <craig@wearegiants.com>
2017-03-07 06:54:39 -0800

    removed relative urls

--- functions.php
+++ functions.php
@@ -29,7 +29,7 @@ function my_acf_settings_dir( $dir ) {
 }
 add_filter('acf/settings/dir', 'my_acf_settings_dir');
 
-add_theme_support('soil-relative-urls');
+//add_theme_support('soil-relative-urls');
 add_theme_support('soil-nice-search');
 add_theme_support('soil-clean-up');
 add_theme_support('soil-google-analytics', 'UA-69397917-1');
